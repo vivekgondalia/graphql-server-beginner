ab987ae4deda626021f9b6013bc69c2bcfe4240b
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2024-05-22 17:41:47 -0700

    fetching all reviews, games, authors

---
+++ dist/index.js
@@ -0,0 +1,80 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+//db
+let games = [
+    { id: '1', title: 'Zelda, Tears of the Kingdom', platform: ['Switch'] },
+    { id: '2', title: 'Final Fantasy 7 Remake', platform: ['PS5', 'Xbox'] },
+    { id: '3', title: 'Elden Ring', platform: ['PS5', 'Xbox', 'PC'] },
+    { id: '4', title: 'Mario Kart', platform: ['Switch'] },
+    { id: '5', title: 'Pokemon Scarlet', platform: ['PS5', 'Xbox', 'PC'] },
+];
+let authors = [
+    { id: '1', name: 'mario', verified: true },
+    { id: '2', name: 'yoshi', verified: false },
+    { id: '3', name: 'peach', verified: true },
+];
+let reviews = [
+    { id: '1', rating: 9, content: 'lorem ipsum', author_id: '1', game_id: '2' },
+    { id: '2', rating: 10, content: 'lorem ipsum', author_id: '2', game_id: '1' },
+    { id: '3', rating: 7, content: 'lorem ipsum', author_id: '3', game_id: '3' },
+    { id: '4', rating: 5, content: 'lorem ipsum', author_id: '2', game_id: '4' },
+    { id: '5', rating: 8, content: 'lorem ipsum', author_id: '2', game_id: '5' },
+    { id: '6', rating: 7, content: 'lorem ipsum', author_id: '1', game_id: '2' },
+    { id: '7', rating: 10, content: 'lorem ipsum', author_id: '3', game_id: '1' },
+];
+//types
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+//scalar types : int, float, string, boolean, ID
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+  type Game {
+    id : ID!
+    title: String!
+    platform: [String!]!
+  }
+
+  type Review {
+    id : ID!
+    rating : Int!
+    content : String!
+  }
+
+  type Author {
+    id : ID!
+    name : String!
+    verified : Boolean
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. THIS IS THE ENTRY POINT INTO THE GRAPHs.
+  type Query {
+    reviews : [Review]
+    games : [Game]
+    authors : [Author]
+  }
+`;
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+        games: () => games,
+        reviews: () => reviews,
+        authors: () => authors
+    }
+};
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at: ${url}`);
